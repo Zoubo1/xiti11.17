a784f6dda226747f411a7838764d479400426db1
Zoubo1 <54834829+Zoubo1@users.noreply.github.com>
2019-11-26 16:32:13 +0800

    Add files via upload

---
+++ jiafaceshi.java
@@ -0,0 +1,29 @@
+package zou;
+	import java.util.Scanner;
+	public class jiafaceshi {
+
+		public static void main(String[] args) {
+			int number1 = (int)(Math.random()*10);
+			int number2 = (int)(Math.random()*10);
+			Scanner input = new Scanner(System.in);
+			System.out.print(
+					"What is"+number1 +"+"+number2+"?");
+			int answer = input.nextInt();
+
+
+			while (number1 +number2 !=answer){
+				
+				System.out.print("Wrong answer.Try again.What is "+number1+"+"+number2+"?");
+					answer = input.nextInt();	
+					while (number1 +number2 !=answer){
+						
+						System.out.print("You already entered .What is "+number1+"+"+number2+"?");
+							answer = input.nextInt();	
+					}
+			}
+
+	System.out.println("You got it!");
+		}
+
+	}
+
